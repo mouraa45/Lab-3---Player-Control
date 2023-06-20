6df25d0ac96c0af9786ca8f59c224fd7889b57c2
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-20 23:07:06 +0100

    termindao limoeza de codigo, e exportaçao  de assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,32 +8,51 @@ public class PlayerController : MonoBehaviour
 	private float speed =10f;
 	private Rigidbody playerRb;
 	private float zBound =6;
-    // Start is called before the first frame update
-    void Start()
-    {
-    	playerRb=GetComponent<Rigidbody>();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		playerRb=GetComponent<Rigidbody>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-    	float horizontalInput=Input.GetAxis("Horizontal");
-    	float verticalInput=Input.GetAxis("Vertical");
-    	
-    	playerRb.AddForce(Vector3.forward * speed * horizontalInput);
-    	playerRb.AddForce(Vector3.left * speed * verticalInput);
-    	   
-    	if(transform.position.z < -zBound)
-    	{
-    		transform.position =new Vector3(transform.position.x, transform.position.y, -zBound);
-    	
-    	}
-    	
-    	if(transform.position.z < zBound)
-    	{
-    		transform.position =new Vector3(transform.position.x, transform.position.y, zBound);
-    	
-    	}
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		MovePlayer();
+		ConstarinPlayerPosition();
+		
+	}
+	
+	
+	
+	//moves player with arrow key
+	void MovePlayer()
+	{
+		float horizontalInput=Input.GetAxis("Horizontal");
+		float verticalInput=Input.GetAxis("Vertical");
+		
+		playerRb.AddForce(Vector3.forward * speed * verticalInput);
+		playerRb.AddForce(Vector3.right * speed * horizontalInput);
+	}
+	
+	// prenvent the player from leaving the top or botton of screen
+	void ConstarinPlayerPosition()
+	{
+		if(transform.position.z < -zBound)
+	{
+		transform.position =new Vector3(transform.position.x, transform.position.y, -zBound);
+		
+	}
+	
+	if(transform.position.z < zBound)
+	{
+		transform.position =new Vector3(transform.position.x, transform.position.y, zBound);
+		
+	}
 }
+
+}
+
+
+
+
+
